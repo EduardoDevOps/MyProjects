bf333783ea40455b772c4950e1359d7196ca536e
U-DATTO\eduardo.meza <eduardo.meza@KA-GWYVMD3-2.datto.lan>
2024-01-03 05:59:08 -0500

    setting up knapsack functions

---
+++ knapsack.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+static const int LIMIT = 50; 
+static const int MINLEN = 3;
+static const int MAXLEN = 1000; 
+
+class knapsack 
+{
+	
+	public: 
+	
+	private:
+		int itemCount; 
+		int* values; 
+		int* sizes; 
+};
